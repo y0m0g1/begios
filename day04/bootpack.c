b506a21d083325e3d14ea9235e0bae3e78022e4d
shinji <6e1a-yomogi@g.ecc.u-tokyo.ac.jp>
2020-08-20 19:45:08 +0900

    d04 using pointer unless write_mem8() function

--- day04/bootpack.c
+++ day04/bootpack.c
@@ -5,10 +5,13 @@ void write_mem8(int addr, int data);
 void HariMain(void)
 {
     int i;
+    char *p;
 
-    for (i = 0xa0000; i <= 0xaffff; i++)
+    p = (char *) 0xa0000;
+
+    for (i = 0; i <= 0xffff; i++)
     {
-        write_mem8(i, i & 0x0f);
+        p[i] = i & 0x0f;
     }
     
 
