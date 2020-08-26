eae5d01a04b377898d48adfd6cacb60d5bbfaa69
shinji <6e1a-yomogi@g.ecc.u-tokyo.ac.jp>
2020-08-26 22:44:33 +0900

    d09 add some functions for memory management

--- day09/bootpack.c
+++ day09/bootpack.c
@@ -6,11 +6,12 @@ extern struct FIFO8 keyfifo, mousefifo;
 // main function
 void HariMain(void)
 {
-    struct BOOTINFO *binfo = (struct BOOTINFO *) 0x0ff0;
+    struct BOOTINFO *binfo = (struct BOOTINFO *) ADR_BOOTINFO;
     char s[40], mcursor[256], keybuf[32], mousebuf[128];
     int mx, my, i;
-    unsigned char mouse_dbuf[3], mouse_phase;
+    unsigned int memtotal;
     struct MOUSE_DEC mdec;
+    struct MEMMAN *memman = (struct MEMMAN *) MEMMAN_ADDR;
 
     init_gdtidt();
     init_pic();
@@ -21,6 +22,11 @@ void HariMain(void)
     io_out8(PIC1_IMR, 0xef);
 
     init_keyboard();
+    enable_mouse(&mdec);
+    memtotal = memtest(0x00400000, 0xbfffffff);
+    memman_init(memman);
+    memman_free(memman, 0x00001000, 0x0009e000);
+    memman_free(memman, 0x00400000, memtotal-0x00400000);
     
     init_palette();
     init_screen(binfo->vram, binfo->scrnx, binfo->scrny);
@@ -32,8 +38,8 @@ void HariMain(void)
     sprintf(s, "(%d, %d)", mx, my);
     putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
 
-    enable_mouse(&mdec);
-    mouse_phase = 0;
+    sprintf(s, "memory:%dMB    free:%dKB", memtotal/(1024*1024), memman_total(memman)/1024);
+    putfonts8_asc(binfo->vram, binfo->scrnx, 0, 32, COL8_FFFFFF, s);
 
     for (;;) 
     {
@@ -102,4 +108,154 @@ void HariMain(void)
         }
         
     }
+}
+
+unsigned int memtest(unsigned int start, unsigned int end)
+{
+    char flag486 = 0;
+    unsigned int eflag, cr0, i;
+
+    eflag = io_load_eflags();
+    eflag |= EFLAGS_AC_BIT;
+    io_store_eflags(eflag);
+    eflag = io_load_eflags();
+    if ((eflag & EFLAGS_AC_BIT) != 0) // if 386 ac bit will be 0 automatically 
+    {
+        flag486 = 1;
+    }
+    eflag &= ~EFLAGS_AC_BIT;
+    io_store_eflags(eflag);
+    if (flag486 != 0)
+    {
+        cr0 = load_cr0();
+        cr0 |= CR0_CACHE_DISABLE; // cache disable
+        store_cr0(cr0);
+    }
+    i = memtest_sub(start, end);
+    if (flag486 != 0)
+    {
+        cr0 = load_cr0();
+        cr0 &= ~CR0_CACHE_DISABLE; // cache enable
+        store_cr0(cr0);
+    }
+
+    return i;
+}
+
+/* initialize memory manager */
+void memman_init(struct MEMMAN *man)
+{
+    man->frees = 0;     
+    man->maxfrees = 0;
+    man->lostsize = 0;  // sum-size which failed to free
+    man->losts = 0;     // counts failed to free
+    return;
+}
+
+/* return the total free size */
+unsigned int memman_total(struct MEMMAN *man)
+{
+    unsigned int i, t=0;
+    for ( i = 0; i < man->frees; i++)
+    {
+        t += man->free[i].size;
+    }
+    return t;
+}
+
+/* memory allocate */
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size)
+{
+    unsigned int i, a;
+    for ( i = 0; i < man->frees; i++)
+    {
+        if (man->free[i].size >= size)
+        {
+            a = man->free[i].addr;
+            man->free[i].addr += size;
+            man->free[i].size -= size;
+            if (man->free[i].size == 0)
+            {
+                // move over
+                man->frees--;
+                for ( ; i < man->frees; i++)
+                {
+                    man->free[i] = man->free[i+1];
+                }
+            }
+            return a;
+        }
+    }
+    return 0; // no space
+}
+
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size)
+{
+    int i,j;
+    for ( i = 0; i < man->frees; i++)
+    {
+        if (man->free[i].addr > addr)
+        {
+            break;
+        }
+        
+    }
+    // free[i-1].addr < addr < free[i].addr
+    if (i > 0)
+    {
+        // integrate with the previous
+        if (man->free[i-1].addr + man->free[i-1].size == addr)
+        {
+            man->free[i-1].size += size;
+            if (i < man->frees)
+            {
+                // also integrate with the next
+                if (addr + size == man->free[i].addr)
+                {
+                    man->free[i-1].size += man->free[i].size;
+                    man->frees--;
+                    for ( ; i < man->frees; i++)
+                    {
+                        man->free[i] = man->free[i+1];
+                    }
+                    
+                }
+                
+            }
+            return 0; // succeed
+        }
+    }
+    // cannot integrate with the previous
+    if (i < man->frees)
+    {
+        // integrate with the next free space (not the previous one)
+        if (addr + size == man->free[i].addr)
+        {
+            man->free[i].addr = addr;
+            man->free[i].size += size;
+            return 0; // succeed
+        }
+    }
+    // cannot integrate with the prev/next
+    if (man->frees < MEMMAN_FREES)
+    {
+        // shift to the back
+        for ( j = man->frees; j > i; j--)
+        {
+            man->free[j] = man->free[j-1];
+        }
+        man->frees++;
+        if (man->maxfrees < man->frees)
+        {
+            man->maxfrees = man->frees;
+        }
+        man->free[i].addr = addr;
+        man->free[i].size = size;
+        return 0; // succeed
+    }
+    // failed to shift
+    man->losts++;
+    man->lostsize += size;
+    return -1; // failed
+    
 }
--- day09/bootpack.h
+++ day09/bootpack.h
@@ -26,11 +26,32 @@ void load_idtr(int limit, int addr);
 void asm_inthandler21(void);
 void asm_inthandler27(void);
 void asm_inthandler2c(void);
+int load_cr0(void);
+void store_cr0(int cr0);
+unsigned int memtest_sub(unsigned int start, unsigned int end);
 
 /***************************************/
 
 // bootpack.c
 
+#define EFLAGS_AC_BIT       0x00040000
+#define CR0_CACHE_DISABLE   0x60000000
+#define MEMMAN_FREES        4090
+#define MEMMAN_ADDR         0x003c0000
+
+struct FREEINFO {
+    unsigned int addr, size;
+};
+struct MEMMAN {
+    int frees, maxfrees, lostsize, losts;
+    struct FREEINFO free[MEMMAN_FREES];
+};
+
+unsigned int memtest(unsigned int start, unsigned int end);
+void memman_init(struct MEMMAN *man);
+unsigned int memman_total(struct MEMMAN *man);
+unsigned int memman_alloc(struct MEMMAN *man, unsigned int size);
+int memman_free(struct MEMMAN *man, unsigned int addr, unsigned int size);
 
 /***************************************/
 
