d31b2be3ba29c6d282b78fae6ea30c91b16fbfda
shinji <6e1a-yomogi@g.ecc.u-tokyo.ac.jp>
2020-08-31 02:24:16 +0900

    d13 introduce a sentinel

--- day13/timer.c
+++ day13/timer.c
@@ -5,17 +5,21 @@ struct TIMERCTL timerctl;
 void init_pit(void)
 {
     int i;
+    struct TIMER *t;
     io_out8(PIT_CTRL, 0x34);
     io_out8(PIT_CNT0, 0x9c);
     io_out8(PIT_CNT0, 0x2e);
     timerctl.count = 0;
-    timerctl.next = 0xffffffff;
-    timerctl.using = 0;
     for (i = 0; i < MAX_TIMER; i++)
     {
         timerctl.timers0[i].flags = 0;
     }
-    
+    t = timer_alloc();
+    t->timeout = 0xffffffff;
+    t->flags = TIMER_FLAGS_USING;
+    t->next = 0; // last item
+    timerctl.t0 = t;
+    timerctl.next = 0xffffffff;
     return;
 }
 
@@ -54,15 +58,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
     timer->flags = TIMER_FLAGS_USING;
     e = io_load_eflags();
     io_cli();
-    timerctl.using++;
-    if (timerctl.using == 1)
-    {
-        timerctl.t0 = timer;
-        timer->next = 0;
-        timerctl.next = timer->timeout;
-        io_store_eflags(e);
-        return;
-    }
     t = timerctl.t0;
     if (timer->timeout <= t->timeout)
     {
@@ -76,10 +71,6 @@ void timer_settime(struct TIMER *timer, unsigned int timeout)
     {
         s = t;
         t = t->next;
-        if (t == 0)
-        {
-            break;
-        }
         if (timer->timeout <= t->timeout)
         {
             s->next = timer;
@@ -105,7 +96,7 @@ void inthandler20(int *esp)
         return;
     }
     timer = timerctl.t0;
-    for (i = 0; i < timerctl.using; i++)
+    for (;;)
     {
         if (timer->timeout > timerctl.count)
         {
@@ -116,16 +107,7 @@ void inthandler20(int *esp)
         fifo32_put(timer->fifo, timer->data);
         timer = timer->next;
     }
-    timerctl.using -= i;
     timerctl.t0 = timer;
-    if (timerctl.using > 0)
-    {
-        timerctl.next = timerctl.t0->timeout;
-    }
-    else
-    {
-        timerctl.next = 0xffffffff;
-    }
-    
+    timerctl.next = timerctl.t0->timeout;
     return;
 }
