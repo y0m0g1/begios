2987120e80d9caa331dfe1707626209a4e09720f
shinji <6e1a-yomogi@g.ecc.u-tokyo.ac.jp>
2020-09-09 15:35:26 +0900

    d19 add cat command

---
+++ day19/fifo.c
@@ -0,0 +1,64 @@
+#include "bootpack.h"
+
+/* intialize fifo buffer */
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf, struct TASK *task)
+{
+    fifo->size = size;
+    fifo->buf = buf;
+    fifo->free = size;
+    fifo->flags = 0;
+    fifo->p = 0; // read label
+    fifo->q = 0; // write label
+    fifo->task = task;
+    return;
+}
+
+/* put and store a data in fifo buffer */
+int fifo32_put(struct FIFO32 *fifo, int data)
+{
+    if (fifo->free == 0)
+    {
+        fifo->flags |= FLAGS_OVERRUN; // overruned because of no free space
+        return -1;
+    }
+    fifo->buf[fifo->p++] = data;
+    if (fifo->p == fifo->size)
+    {
+        fifo->p = 0;
+    }
+    fifo->free--;
+    // wake the task up
+    if (fifo->task != 0)
+    {
+        if (fifo->task->flags != 2)
+        {
+            task_run(fifo->task, -1, 0);
+        }
+    }
+    return 0;
+    
+}
+
+/* get a data from fifo buffer */
+int fifo32_get(struct FIFO32 *fifo)
+{
+    int data;
+    if (fifo->free == fifo->size)
+    {
+        // if empty, retun -1
+        return -1;
+    }
+    data = fifo->buf[fifo->q++];
+    if (fifo->q == fifo->size)
+    {
+        fifo->q = 0;
+    }
+    fifo->free++;
+    return data;    
+}
+
+// return how much data are stored
+int fifo32_status(struct FIFO32 *fifo)
+{
+    return fifo->size - fifo->free;
+}
---
+++ day19/keyboard.c
@@ -0,0 +1,39 @@
+#include "bootpack.h"
+
+struct FIFO32 *keyfifo;
+int keydata0;
+
+/* interrupt from PS/2 keyboard */
+void inthandler21(int *esp)
+{
+    int data;
+    io_out8(PIC0_OCW2, 0x61);
+    data = io_in8(PORT_KEYDAT);
+    fifo32_put(keyfifo, data+keydata0);
+    return;
+}
+
+/* initialize keyboard controller*/
+void init_keyboard(struct FIFO32 *fifo, int data0)
+{
+    keyfifo = fifo;
+    keydata0 = data0;
+    wait_KBC_sendready();
+    io_out8(PORT_KEYCMD, KEYCMD_WRITE_MODE);
+    wait_KBC_sendready();
+    io_out8(PORT_KEYDAT, KBC_MODE);
+    return;
+}
+
+void wait_KBC_sendready(void)
+{
+    for (;;)
+    {
+        if ((io_in8(PORT_KEYSTA) & KEYSTA_SEND_NOTREADY)==0)
+        {
+            break;
+        }
+        
+    }
+    return;
+}
