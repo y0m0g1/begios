9ed9f325de1781cf814be106e857b08f832b56e1
shinji <6e1a-yomogi@g.ecc.u-tokyo.ac.jp>
2020-09-09 17:09:20 +0900

    d19 run a new application

--- day19/console.c
+++ day19/console.c
@@ -6,6 +6,7 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
     struct TASK *task = task_now();
     struct MEMMAN *memman = (struct MEMMAN *)MEMMAN_ADDR;
     struct FILEINFO *finfo = (struct FILEINFO *) (ADR_DISKIMG+0x002600);
+    struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) ADR_GDT;
     int i, fifobuf[128], cursor_x=16, cursor_y=28,cursor_c=COL8_000000;
     int x, y;
     int *fat = (int *) memman_alloc_4k(memman, 4*2880);
@@ -224,6 +225,56 @@ void console_task(struct SHEET *sheet, unsigned int memtotal)
                                 }
                             }
                             memman_free_4k(memman, (int) p, finfo[x].size);
+                        }
+                        else
+                        {
+                            // not found
+                            putfonts8_asc_sht(sheet, 8, cursor_y, COL8_FFFFFF, COL8_000000, "file not found.", 15);
+                            cursor_y = cons_newline(cursor_y, sheet);
+                        }
+                        cursor_y = cons_newline(cursor_y, sheet);
+                    }
+                    else if (strcmp(cmdline, "hlt") == 0)
+                    {
+                        // run hlt.hub application
+                        for (y = 0; y < 11; y++)
+                        {
+                            s[y] = ' ';
+                        }
+                        s[0] = 'H';
+                        s[1] = 'L';
+                        s[2] = 'T';
+                        s[8] = 'H';
+                        s[9] = 'R';
+                        s[10] = 'B';
+                        for (x = 0; x < 224;)
+                        {
+                            if (finfo[x].name[0] == 0x00)
+                            {
+                                break;
+                            }
+                            if ((finfo[x].type & 0x18) == 0)
+                            {
+                                for (y = 0; y < 11; y++)
+                                {
+                                    if (finfo[x].name[y] != s[y])
+                                    {
+                                        goto hlt_next_file;
+                                    }
+                                }
+                                break;
+                            }
+                            hlt_next_file:
+                            x++;
+                        }
+                        if (x < 224 && finfo[x].name[0] != 0x00)
+                        {
+                            // found
+                            p = (char *) memman_alloc_4k(memman, finfo[x].size);
+                            file_loadfile(finfo[x].clustno, finfo[x].size, p, fat, (char *)(ADR_DISKIMG+0x003e00));
+                            set_segmdesc(gdt+1003, finfo[x].size-1, (int)p, AR_CODE32_ER);
+                            farjmp(0, 1003*8);
+                            memman_free_4k(memman, (int)p, finfo[x].size);
                         }
                         else
                         {
